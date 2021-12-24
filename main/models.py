594db3c0c84a60bc1208cffdc13e9ff8306ca9e7
Tima <timur@Tim.local>
2021-12-24 10:29:40 +0600

    create django project-models

---
+++ main/models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('main:category_list', args=[self.slug])
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=150, default='admin')
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=150)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Products'
+        ordering = ('-created',)
+
+    def get_absolute_url(self):
+        return reverse('main:product_detail', args=[self.slug])
+
+    def __str__(self):
+        return self.title
